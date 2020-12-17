541b4f8b5a10453c2914211cc0a59fbd2bc38cab
Audun Torp <audun@zedge.net>
2020-12-17 23:36:34 +0100

    Working quite well

--- text.js
+++ text.js
@@ -1,6 +1,4 @@
-function boros(id, text, url) {
-    const name = id
-    document.body.innerHTML = document.body.innerHTML.replace(text, '<span id=' + name + ' class="zedgestorm-keyword">' + text + '</span>')
+function boros(name, text, url) {
     let boros = document.createElement('div')
     boros.setAttribute("id", "zedgestorm-" + name)
     boros.setAttribute("class", "ui-widget-content")
@@ -9,28 +7,31 @@ function boros(id, text, url) {
     boros.append(img)
     document.body.appendChild(boros)
 
-    $( "#" + name ).mousemove(function(e) {
-        console.log(name)
-        let boros = $( "#zedgestorm-" + name )
-        boros.css({display: "block"})
-        boros.position({
-            my: "left+10 bottom",
+    let keyword = $( "#" + name );
+    let card = $( "#zedgestorm-" + name );
+    keyword.mouseenter(function (e) {
+        card.css({display: "block"})
+    });
+    keyword.mousemove(function (e) {
+        card.position({
+            my: "left+10 top",
             of: e.originalEvent,
             collision: "flip"
         });
     });
-    function addTimeout() {
-        let boros = $( "#zedgestorm-" + name )
-        boros.remove_timeout = setTimeout(function() {
-            boros.css({display: "none"})
+
+    var timeout = {};
+    const addTimeout = function() {
+        timeout[name] = setTimeout(function() {
+            card.css({display: "none"})
         }, 3000)
     }
 
-    $( "#" + name ).mouseleave(addTimeout);
-    $( "#zedgestorm-" + name ).mouseleave(addTimeout);
-    $( "#zedgestorm-" + name ).mousemove(function() {
-        clearTimeout($("#zedgestorm-" + name).remove_timeout)
+    keyword.mouseleave(addTimeout);
+    card.mousemove(function() {
+        clearTimeout(timeout[name])
     });
+    card.mouseleave(addTimeout);
 }
 
 
@@ -50,8 +51,18 @@ window.addEventListener('load', (event) => {
         classname[i].addEventListener('mouseenter', animateButton, false);
     }
 
-    boros("boros", 'Boros Guildgate', "https://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=366390&type=card");
-    boros("chemister", 'Chemister\'s Insight', "https://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=482736&type=card");
-    boros("capture", 'Capture Sphere', "https://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=485370&type=card");
-
+    let cards = [
+        ["chemister", 'Chemister\'s Insight', "https://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=482736&type=card"],
+        ["capture", 'Capture Sphere', "https://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=485370&type=card"],
+        ["boros", 'Boros Guildgate', "https://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=366390&type=card"]
+    ]
+    for (i in cards) {
+        let card = cards[i]
+        console.log(card)
+        document.body.innerHTML = document.body.innerHTML.replaceAll(card[1], '<span id=' + card[0] + ' class="zedgestorm-keyword">' + card[1] + '</span>')
+    }
+    for (i in cards) {
+        let card = cards[i]
+        boros(card[0], card[1], card[2])
+    }
 });
